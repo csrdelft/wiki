2677aaae8bb9f91608ee0c99e7150a5e292425be
Gerben Oolbekkink <gerben@bunq.com>
2017-05-26 16:33:24 +0200

    Fix probleem beter

--- lib/exe/ajax.php
+++ lib/exe/ajax.php
@@ -7,7 +7,7 @@
  */
 
 if(!defined('DOKU_INC')) define('DOKU_INC',dirname(__FILE__).'/../../');
-if(!defined('DOKU_UNITTEST_ASSUME_WINDOWS')) define('DOKU_UNITTEST_ASSUME_WINDOWS',0);
+if(!key_exists('DOKU_UNITTEST_ASSUME_WINDOWS', @$GLOBALS)) @$GLOBALS['DOKU_UNITTEST_ASSUME_WINDOWS'] = false;
 require_once(DOKU_INC.'inc/init.php');
 //close session
 session_write_close();

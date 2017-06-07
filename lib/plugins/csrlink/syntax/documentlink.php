39aaa922a93d4ac67e908aa28f053cbffb5fe9c9
Gerben Oolbekkink <gerben@bunq.com>
2017-06-07 19:22:25 +0200

    Fix documentlink

--- lib/plugins/csrlink/syntax/documentlink.php
+++ lib/plugins/csrlink/syntax/documentlink.php
@@ -128,7 +128,7 @@ class syntax_plugin_csrlink_documentlink extends DokuWiki_Syntax_Plugin {
         $class = preg_replace('/[^_\-a-z0-9]+/i','_',$ext);
 
         //return html
-		$renderer->doc .= '<a href="'.$documenturl.'" class="documentlink csrlink_plugin mediafile mf_'.$class.'">'.hsc($title).'</a> <span class="documentlink csrlink_plugin size">('.CsrDelft\format_filesize((int)$document->getFileSize()).')</span>';
+		$renderer->doc .= '<a href="'.$documenturl.'" class="documentlink csrlink_plugin mediafile mf_'.$class.'">'.hsc($title).'</a> <span class="documentlink csrlink_plugin size">('.CsrDelft\format_filesize((int)$document->filesize).')</span>';
 
         return true;
     }
